c9e88a38696d615255ba79223222961ac6acc281
hwinkr <dnddl8280@naver.com>
2023-08-31 10:33:32 +0900

    Config: 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import DetailIssue from './pages/DetailIssue';
+import PATH from './constants/path';
+import IssueProvider from './components/Provider/IssueProvider';
+import Issues from './pages/Issues';
+import Error from './pages/Error';
 
 function App() {
-  return <>App</>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path={PATH.ROOT} element={<Home />} />
+        <Route element={<IssueProvider />}>
+          <Route path={PATH.ISSUES} element={<Issues />} />
+        </Route>
+        <Route path={PATH.DETAIL_ISSUE} element={<DetailIssue />} />
+        <Route
+          path={PATH.ERROR}
+          element={<Error imagePath="/assets/Error404.jpg" error="Page Not Found!" />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
