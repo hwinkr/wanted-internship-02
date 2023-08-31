c845ce1ae11e277fde19fad941279a9da3a0fca2
hwinkr <dnddl8280@naver.com>
2023-08-31 10:30:36 +0900

    Chore: 상수 설정

---
+++ src/constants/header-title.ts
@@ -0,0 +1,7 @@
+const HEADER_TITLE = {
+  HOME: 'Git Hub Issues - React',
+  ISSUES: 'facebook/react',
+  ERROR: 'Error Page',
+};
+
+export default HEADER_TITLE;
---
+++ src/constants/path.ts
@@ -0,0 +1,8 @@
+const PATH = {
+  ROOT: '/',
+  ISSUES: '/issues',
+  DETAIL_ISSUE: '/issues/:issueNumber',
+  ERROR: '*',
+} as const;
+
+export default PATH;
